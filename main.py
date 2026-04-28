c192967460efd9206bfe09e1ed6b4d6cf4144392
dvaJf <dvakgj@gmail.com>
2026-04-28 15:49:57 +0300

    1

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="."), name="static")
+
 @app.get("/")
 async def serve_index():
     return FileResponse("1.html")
